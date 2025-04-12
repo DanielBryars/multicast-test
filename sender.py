38b7aefe46af2153d9d72ac29fb3ca4eb5046a6f
Daniel Bryars <bryars@hotmail.com>
2025-04-12 15:43:26 +0100

    Now sends a counter and the name of the sender

--- sender.py
+++ sender.py
@@ -10,7 +10,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 ttl = struct.pack('b', 1)  # Stay within local network
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
+i=0
 while True:
-    sock.sendto(b"Hello multicast", (MCAST_GRP, MCAST_PORT))
+    i=i+1
+    hostname = socket.gethostname()
+    message = f"Hello Multicast:{i} from:'{hostname}'"
+    sock.sendto(message.encode("utf-8"), (MCAST_GRP, MCAST_PORT))
     print("Sent")
     time.sleep(1)
